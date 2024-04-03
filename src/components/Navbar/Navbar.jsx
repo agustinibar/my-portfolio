c44b7d74d4266efaea06022a163933dc8b87513b
agustinibar <agustinibarperrotta@gmail.com>
2024-04-03 15:12:37 -0300

    navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,25 @@
-import './navbar.scss'
+import "./navbar.scss"
+
 import { Twitter } from '@mui/icons-material';
 import { LinkedIn } from '@mui/icons-material';
 import { WhatsApp } from '@mui/icons-material';
 
 export const Navbar = () => {
   return (
-    <div className='navbar'>
+    <div className="navbar">
         {/* Sidebar */}
-        <div className='wrapper'>
+        <div className="wrapper">
             <span>Ibar Agustin</span>
-            <div className='social'>
+            <div className="social">
+                <a href="#">
                 <Twitter/>
+                </a>
+                <a href="#">
                 <LinkedIn/>
+                </a>
+                <a href="#">
                 <WhatsApp/>
+                </a>
             </div>
         </div>
     </div>

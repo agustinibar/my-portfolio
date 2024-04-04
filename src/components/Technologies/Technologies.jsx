87d1b64eec68aaf8fc9ae4d5f646cdebdbd4cfe9
agustinibar <agustinibarperrotta@gmail.com>
2024-04-04 18:55:10 -0300

    technologies h1

--- src/components/Technologies/Technologies.jsx
+++ src/components/Technologies/Technologies.jsx
@@ -8,13 +8,40 @@ const Technologies = () => {
       <motion.div className="textContainer">
         <p>I am a fullstack developer and I am focused 
           <br />on specializing in back-end</p>
+          <hr />
       </motion.div>
       <motion.div className="tittleContainer">
           <div className="tittle">
-            <img src="" alt="" />
+            <img src="/people.webp" alt="" />
+            <h1><b>Mastered</b> technology &</h1>
+          </div>
+          <div className="tittle">  
+            <h1><b>Path In</b> Development</h1>
+            <button>WHAT CAN I DO?</button>
           </div>
       </motion.div>
-      <motion.div className="listContainer"></motion.div>
+      <motion.div className="listContainer">
+        <div className="box">
+          <h2>Frontend</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit totam molestiae voluptatem natus! Alias cupiditate eligendi accusamus officia doloribus laudantium, labore, ad soluta expedita vitae iste, unde ipsum aspernatur pariatur!</p>
+          <button>Go</button>
+        </div>
+        <div className="box">
+          <h2>Backend</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit totam molestiae voluptatem natus! Alias cupiditate eligendi accusamus officia doloribus laudantium, labore, ad soluta expedita vitae iste, unde ipsum aspernatur pariatur!</p>
+          <button>Go</button>
+        </div>
+        <div className="box">
+          <h2>Design</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit totam molestiae voluptatem natus! Alias cupiditate eligendi accusamus officia doloribus laudantium, labore, ad soluta expedita vitae iste, unde ipsum aspernatur pariatur!</p>
+          <button>Go</button>
+        </div>
+        <div className="box">
+          <h2>Branding</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit totam molestiae voluptatem natus! Alias cupiditate eligendi accusamus officia doloribus laudantium, labore, ad soluta expedita vitae iste, unde ipsum aspernatur pariatur!</p>
+          <button>Go</button>
+        </div>
+      </motion.div>
     </motion.div>
   )
 }

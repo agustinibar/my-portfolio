8828ea33d9a72bf06b94a9c9d138f9ab4e4c4a80
agustinibar <agustinibarperrotta@gmail.com>
2024-04-04 17:33:32 -0300

    parallax

--- src/components/Parallax/Parallax.jsx
+++ src/components/Parallax/Parallax.jsx
@@ -1,17 +1,35 @@
+import { useRef } from 'react';
 import './parallax.scss';
 
+import { motion, useScroll, useTransform } from 'framer-motion';
+
 const Parallax = ({ type }) => {
+  
+  const ref = useRef();
+
+  const {scrollYProgress} = useScroll({
+    target:ref,
+    offset: ["start start", "end start"]
+  })
+
+  const yText = useTransform(scrollYProgress, [0,1], ["0%", "500%"]);
+  const yBg = useTransform(scrollYProgress, [0,1], ["0%", "200%"]);
+
   const backgroundStyle = type === "Services" 
     ? { background: "linear-gradient(180deg, #0c0c1d, #111132)" }
     : { background: "linear-gradient(180deg, #0c0c1d, #505064)" };
 
   return (
-    <div className="parallax" style={backgroundStyle}>
-        <h1> { type === "Services" ? "What I Do?" : "What I Did?" }</h1>
-        <div className="mountains"></div>
-        <div className="planets"></div>
-        <div className="stars"></div>
-    </div>
+    <motion.div className="parallax" 
+    ref={ref}
+    style={backgroundStyle}>
+        <motion.h1 style={{ y:yText }}> 
+        { type === "Services" ? "What I Do?" : "What I Did?" }
+        </motion.h1>
+        <motion.div className="mountains"></motion.div>
+        <motion.div style={{ y: yBg  }} className="planets"></motion.div>
+        <motion.div style={{ x: yBg  }} className="stars"></motion.div>
+    </motion.div>
   )
 }
 

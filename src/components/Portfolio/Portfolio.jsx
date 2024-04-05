8f8400a9946ba926c5a2cf35bf3046687da0a4a8
agustinibar <agustinibarperrotta@gmail.com>
2024-04-05 12:18:40 -0300

    motion portfolio

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,7 +1,7 @@
 import { useRef } from 'react';
 import './portfolio.scss'
 
-import { motion, useScroll, useSpring  } from 'framer-motion';
+import { motion, useScroll, useSpring, useTransform  } from 'framer-motion';
 
 const items = [
   {
@@ -31,9 +31,30 @@ const items = [
 ];
 
 const Single = ({ item })=>{
+  
+  const ref = useRef();
+
+  const { scrollYProgress } = useScroll({ 
+    target: ref,
+    // offset: ["end end", "start start"]
+  });
+
+  const y =  useTransform( scrollYProgress, [0,1], [-300, 300]);
+
   return(
     <section>
-      {item.tittle}
+      <div className="container">
+        <div className="wrapper">
+          <div className="imageContainer" ref={ref}>
+            <img src={item.img} alt="" />
+          </div>
+        <motion.div className="textContainer" style={{y}}>
+          <h2>{item.tittle}</h2>
+          <p>{item.desc}</p>
+          <button>See Demo</button>
+        </motion.div>
+        </div>
+      </div>
     </section>
   )
 }

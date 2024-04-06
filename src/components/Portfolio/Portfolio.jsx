db46dde231412c6f4104c86a3ced1d08791e6536
agustinibar <agustinibarperrotta@gmail.com>
2024-04-06 18:33:21 -0300

    end

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -13,25 +13,29 @@ const items = [
     id:1,
     tittle: "Dream Lodge",
     img: dreamlodgeImage,
-    desc: "An innovative React project leveraging Firebase technologies for luxury accommodation rentals in the US. With real-time database, storage, and user authentication, it offers a seamless booking experience. Responsive design ensures usability across devices."
+    desc: "An innovative React project leveraging Firebase technologies for luxury accommodation rentals in the US. With real-time database, storage, and user authentication, it offers a seamless booking experience. Responsive design ensures usability across devices.",
+    demo:"https://dreamlodgeprueba.web.app/",
   },
   {
     id:2,
     tittle: "Xapport",
     img: xapportImage,
-    desc: " A dynamic MERN stack application integrating Firebase database to supplement MongoAtlas. Inspired by Twitter, it features user authentication, post creation and viewing, advertising space, friend management, self-help group creation, user chat, and user profile views. Currently in development phase."
+    desc: " A dynamic MERN stack application integrating Firebase database to supplement MongoAtlas. Inspired by Twitter, it features user authentication, post creation and viewing, advertising space, friend management, self-help group creation, user chat, and user profile views. Currently in development phase.",
+    demo:"https://xapport.netlify.app/"
   },
   {
     id:3,
     tittle: "Amuebla",
     img: amueblaImage,
-    desc: "Amuebla is an ecommerce site built using React and Firebase. It features user authentication, product browsing, detailed product pages, and a cart with MercadoPago integration. An admin dashboard allows for product management, user security, and order tracking. The platform is still in development."
+    desc: "Amuebla is an ecommerce site built using React and Firebase. It features user authentication, product browsing, detailed product pages, and a cart with MercadoPago integration. An admin dashboard allows for product management, user security, and order tracking. The platform is still in development.",
+    demo:"https://amuebla-6a3f2.web.app/",
   },
   {
     id:3,
     tittle: "Foodie Portfolio",
     img: foodieImage,
-    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos necessitatibus veritatis ducimus, sapiente iusto eveniet fuga eligendi laudantium excepturi. Doloribus autem cumque dolorum fuga corporis asperiores molestias. Repellendus, architecto dolorem."
+    desc: "Foodie is a web development services company portfolio created with Tailwind and Framer Motion for component animation. It features language toggling between Spanish and English and includes a newsletter section. This simple single-page application (SPA) aims to showcase the best of branding and web development services.",
+    demo:"https://foodiesitelab.com/"
   }
 ];
 
@@ -46,6 +50,10 @@ const Single = ({ item })=>{
 
   const y =  useTransform( scrollYProgress, [0,1], [-300, 300]);
 
+  const handleDemoClick = () => {
+    window.open(item.demo, '_blank');
+  };
+
   return(
     <section>
       <div className="container">
@@ -56,7 +64,7 @@ const Single = ({ item })=>{
         <motion.div className="textContainer" style={{y}}>
           <h2>{item.tittle}</h2>
           <p>{item.desc}</p>
-          <button>See Demo</button>
+          <button onClick={handleDemoClick}>See Demo</button>
         </motion.div>
         </div>
       </div>
